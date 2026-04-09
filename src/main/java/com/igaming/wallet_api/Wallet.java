631cb9a9d19af9dbf5351a07162468ee5003dad0
Serge Menko <w.houmenko@gmail.com>
2026-04-09 20:50:58 +0200

    feat: add WalletController with getBalance endpoint

--- src/main/java/com/igaming/wallet_api/Wallet.java
+++ src/main/java/com/igaming/wallet_api/Wallet.java
@@ -1,9 +1,5 @@
 package com.igaming.wallet_api;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class Wallet {
 
     private String userId;
--- src/main/java/com/igaming/wallet_api/WalletController.java
+++ src/main/java/com/igaming/wallet_api/WalletController.java
@@ -1,5 +1,6 @@
 package com.igaming.wallet_api;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
